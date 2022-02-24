e083c1a3f471479ae37b6af5a9a1f250c0faaff3
Bob <kearnsr135@gmail.com>
2022-02-24 20:36:58 +0000

    added comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function compute()
 {   
+    // gather variables from HTML
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
     var principal = document.getElementById("principal").value;
+    // validate if principal value is greater than zero, else don't compute and focus
     if(principal <= 0){
         alert("Enter a positve number");
         document.getElementById("principal").focus();
@@ -14,6 +16,7 @@ function compute()
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
 }
 
+// way to dynamically update value to changes with slider
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
